ad6574f1501561fe43039c65d9128709661f8c64
Sheik Mostafizur <sheikmostafizur2001@gmail.com>
2024-09-29 20:11:30 +0600

    working good

--- src/routes/get-routes.ts
+++ src/routes/get-routes.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
 import { User } from "../models/user";
+import { Post } from "../models/post";
 
 const router = Router();
 
 // Create a new user
 router.get("/get-all", async (req, res) => {
   try {
-    const user = await User.findAll({ include: [] });
+    const user = await User.findAll({ include: [Post] });
 
-    res.status(201).json({ user });
+    const posts = await Post.findAll({ include: [User] });
+
+    res.status(201).json({ posts, user });
   } catch (error: any) {
     res.status(500).json({ error: error.message });
   }
