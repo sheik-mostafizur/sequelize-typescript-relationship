2a5528ad4cc1e1fce9e1ac115ad746ff3344d92a
Sheik Mostafizur <sheikmostafizur2001@gmail.com>
2024-09-30 23:15:16 +0600

    working with json type

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import sequelize from "./config/db";
 import getAllRoutes from "./routes/get-routes";
+import userRoutes from "./routes/user-routes";
 import { User } from "./models/user";
 import { Post } from "./models/post";
 const app = express();
@@ -8,33 +9,81 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api", [getAllRoutes]);
+app.use("/api", [getAllRoutes, userRoutes]);
 
 const createData = async () => {
-  await User.create({ name: "MD Rohim", email: "romni@gmail.com" });
-  await User.create({ name: "jamel", email: "jamel@gmail.com" });
-  await User.create({ name: "jasmin", email: "jasmin@gmail.com" });
+  await User.create({
+    name: "MD Rohim",
+    email: "romni@gmail.com",
+    address: {
+      Country: "Bangladesh",
+      StateDivision: "Khulna Division",
+      CountyDistrict: "Chuadanga District",
+      CityTown: "Alamda",
+      PostalCode: "12345",
+      StreetHouseNumber: "12/A",
+      present: {
+        CityTown: "Alamda",
+        PostalCode: "54321",
+        StreetHouseNumber: "14/B",
+      },
+    },
+  });
+  // await User.create({
+  //   name: "jamel",
+  //   email: "jamel@gmail.com",
+  //   address: {
+  //     Country: "Bangladesh",
+  //     StateDivision: "Khulna Division",
+  //     CountyDistrict: "Chuadanga District",
+  //     CityTown: "Alamda",
+  //     PostalCode: "12345",
+  //     StreetHouseNumber: "12/A",
+  //     present: {
+  //       CityTown: "Alamda",
+  //       PostalCode: "54321",
+  //       StreetHouseNumber: "14/B",
+  //     },
+  //   },
+  // });
+  // await User.create({
+  //   name: "jasmin",
+  //   email: "jasmin@gmail.com",
+  //   address: {
+  //     Country: "Bangladesh",
+  //     StateDivision: "Khulna Division",
+  //     CountyDistrict: "Chuadanga District",
+  //     CityTown: "Alamda",
+  //     PostalCode: "12345",
+  //     StreetHouseNumber: "12/A",
+  //     present: {
+  //       CityTown: "Alamda",
+  //       PostalCode: "54321",
+  //       StreetHouseNumber: "14/B",
+  //     },
+  //   },
+  // });
 
   await Post.create({ title: "U1 Post 1", userId: 1 });
   await Post.create({ title: "U1 Post 2", userId: 1 });
   await Post.create({ title: "U1 Post 3", userId: 1 });
 
-  await Post.create({ title: "U2 Post 1", userId: 2 });
-  await Post.create({ title: "U2 Post 2", userId: 2 });
-  await Post.create({ title: "U2 Post 3", userId: 2 });
+  // await Post.create({ title: "U2 Post 1", userId: 2 });
+  // await Post.create({ title: "U2 Post 2", userId: 2 });
+  // await Post.create({ title: "U2 Post 3", userId: 2 });
 
-  await Post.create({ title: "U3 Post 1", userId: 3 });
-  await Post.create({ title: "U3 Post 2", userId: 3 });
-  await Post.create({ title: "U3 Post 3", userId: 3 });
+  // await Post.create({ title: "U3 Post 1", userId: 3 });
+  // await Post.create({ title: "U3 Post 2", userId: 3 });
+  // await Post.create({ title: "U3 Post 3", userId: 3 });
 };
 
-// createData()
+// createData();
 
 // Sync database and start server
 const startServer = async () => {
   const PORT = 4000;
   try {
-    await sequelize.sync(); // Sync database
+    await sequelize.sync({ force: false }); // Sync database
     app.listen(PORT, () => {
       console.log("Server is running on http://localhost:" + PORT);
     });
--- src/models/user.ts
+++ src/models/user.ts
@@ -10,10 +10,24 @@ import {
 import { Optional } from "sequelize";
 import { Post } from "./post";
 
+interface Address {
+  Country: string;
+  StateDivision: string;
+  CountyDistrict: string;
+  CityTown: string;
+  PostalCode?: string; // Optional field
+  StreetHouseNumber?: string; // Optional field
+  present: {
+    CityTown: string;
+    PostalCode?: string;
+    StreetHouseNumber?: string;
+  };
+}
 interface UserAttributes {
   id: number;
   name: string;
   email: string;
+  address: Address;
 }
 
 interface UserCreationAttributes extends Optional<UserAttributes, "id"> {}
@@ -36,6 +50,19 @@ export class User extends Model<UserAttributes, UserCreationAttributes> {
 
   @Column(DataType.STRING)
   email!: string;
+
+  @Column({
+    type: DataType.JSON,
+    allowNull: false, // This ensures the address field is not null
+    validate: {
+      hasRequiredFields(value: Address) {
+        if (!value.Country || !value.StateDivision || !value.present.CityTown) {
+          throw new Error("Required fields in address are missing");
+        }
+      },
+    },
+  })
+  address!: Address;
 
   @HasMany(() => Post)
   posts!: Post[]; // One-to-many relationship with Post
